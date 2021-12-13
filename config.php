bf30f5d2b541275d064242f2ae31f0fcb1372887
Froders <69701617+Froders@users.noreply.github.com>
2021-12-13 02:40:06 +0000

    Sorting out database config for website

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 /* Database credentials. Assumption that they haven't been changed */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME','root');
-define('DB_PASSWORD','');
-define('DB_NAME','firstwebsite');
+define('DB_SERVER', 'localhost:3306');
+define('DB_USERNAME',' oxjbdjmy_WPHLZ');
+define('DB_PASSWORD','Froders654');
+define('DB_NAME','oxjbdjmy_salmewebsite');
 
 /*Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
