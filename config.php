528c3885211e4b462d5501812b7f70a3d02ca274
Froders <69701617+Froders@users.noreply.github.com>
2021-12-13 02:43:17 +0000

    removing a space

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 /* Database credentials. Assumption that they haven't been changed */
 define('DB_SERVER', 'localhost:3306');
-define('DB_USERNAME',' oxjbdjmy_WPHLZ');
+define('DB_USERNAME','oxjbdjmy_WPHLZ');
 define('DB_PASSWORD','Froders654');
 define('DB_NAME','oxjbdjmy_salmewebsite');
 
