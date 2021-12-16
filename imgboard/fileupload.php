00cd8a5fe496a27439472dd2e0a9b66faa3f3020
Froders <69701617+Froders@users.noreply.github.com>
2021-12-16 18:21:45 +0000

    Added Options

--- imgboard/fileupload.php
+++ imgboard/fileupload.php
@@ -6,7 +6,17 @@
 <body>
     <form action="upload.php" enctype="multipart/form-data" method="post">
         <label> Select image : </label>
-        <input type="file" name="image">
+        <input type="file" name="image"> <br>
+        <!-- Selection to put into topic for Database for display in view.php-->
+        <select name = "topic">
+        <option value = "Christmas21" selected>Christmas 2021</option>
+            <option value = "azer21">Azerbaijan 2021</option>
+            <option value = "card21">Cardiff 2021</option>
+            <option value = "londoct21" >London October 2021</option>
+            <option value = "londnov21" >London November 2021</option>
+            <option value = "tom2021">Tom's Birthday 2021</option>
+            <option value = "manc2021">Manchester 2021</option>
+        </select>
         <input type="submit" value="Upload" name="submit"> <br/>
     </form>
 
